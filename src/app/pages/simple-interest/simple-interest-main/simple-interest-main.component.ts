d8e93823c8312db3ea88fc1b6819f2ad9f6036f5
Axel Ricardo Ascue Peña <axelrap@outlook.com>
2023-09-25 13:29:20 -0500

    first commit

---
+++ src/app/pages/simple-interest/simple-interest-main/simple-interest-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-interest-main',
+  templateUrl: './simple-interest-main.component.html',
+  styleUrls: ['./simple-interest-main.component.scss']
+})
+export class SimpleInterestMainComponent {
+
+}
